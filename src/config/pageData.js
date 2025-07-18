5766d4c9c9fb9355365604c6adf4ed436a10dfe8
Mari Topel <maritopel@gmail.com>
2025-07-18 15:27:43 -0500

    added commas to pageData.js

--- src/config/pageData.js
+++ src/config/pageData.js
@@ -4,7 +4,7 @@
 export const PAGE_NAMES = {
   "/city": "The Great City of Archadeus",
   "/city/pub": "The Drunken Dragon Tavern",
-  "/city/archery": "Archery Range"
+  "/city/archery": "Archery Range",
   "/desert": "The Sand Snake Expanse",
   "/coast": "The Eldritch Coast",
 };
@@ -14,8 +14,9 @@ export const PAGE_DESCRIPTIONS = {
     "In the Great City of Archadeus you'll find a shop with magical and non magical items, an archery range to learn shooting, a shrine to the great fox, a lively pub, a colorful wish fountain, and the grand royal castle.",
   "/city/pub":
     "In this pub there is a faerie serving drinks behind the bar. There are several yokai playing a game of dice. And there is a troll in the corner drinking by himself. It is cozy in here and everyone seems to be having a good time.",
-  "/city/archery" : "This archery has many targets and plenty of bows and arrows. A man from a distant land is there looking at you expectantly. He probably wants to teach the user how to shoot a bow and arrow."
-    "/desert":
+  "/city/archery":
+    "This archery has many targets and plenty of bows and arrows. A man from a distant land is there looking at you expectantly. He probably wants to teach the user how to shoot a bow and arrow.",
+  "/desert":
     "The Snake Sands is a vast desert of dunes, with a Snake Stone turn-in, protective sand snakes, a hidden oasis, a bustling market of rare wares, and short sand snake rides.",
   "/coast":
     "Along the Eldritch Coast the moonlit waves glow, traders peddle exotic wares on rocky docks, floors teem with mysterious holes, and a witch haven lies ashore.",
